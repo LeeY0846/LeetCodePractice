7b3d4058ba09b5d2bf1cb9aac955872ba3c12bf4
Win11-R9P <ylee091126@163.com>
2023-12-25 17:37:04 +0800

    Solution for 91 using recursion

--- Practice/Practice.cpp
+++ Practice/Practice.cpp
@@ -6,6 +6,7 @@
 #include <map>
 #include <unordered_map>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -120,13 +121,13 @@ public:
     // Get the smaller value between nums1[p1] and nums2[p2] and move the pointer forward.
 
     int getMin(vector<int>& nums1, vector<int>& nums2) {
-        if (p1 < nums1.size() && p2 < nums2.size()) {
+        if (p1 < (int)nums1.size() && p2 < (int)nums2.size()) {
             return nums1[p1] < nums2[p2] ? nums1[p1++] : nums2[p2++];
         }
-        else if (p1 < nums1.size()) {
+        else if (p1 < (int)nums1.size()) {
             return nums1[p1++];
         }
-        else if (p2 < nums2.size()) {
+        else if (p2 < (int)nums2.size()) {
             return nums2[p2++];
         }
         return -1;
@@ -155,57 +156,112 @@ public:
 
 };
 
-int main()
-{
-    cout << "Hello" << endl;
-}
 
-class MyCircularQueue {
+class Solution_Open_The_Lock {
 public:
-    vector<int> values;
-    int max_capacity;
-    int start_p = 0, end_p = 1;
-
-
-    MyCircularQueue(int k) {
-        values = vector<int>();
-        values.reserve(k);
-        max_capacity = k;
-    }
-
-    bool enQueue(int value) {
-        if (isFull()) return false;
-        values[end_p] = value;
-        end_p += 1;
-        if (end_p == max_capacity) end_p = 0;
-        return true;
-    }
-
-    bool deQueue() {
-        if (isEmpty()) return false;
-        start_p += 1;
-        if (start_p == max_capacity) start_p = 0;
-        return true;
-    }
-
-    int Front() {
-        return values[start_p];
-    }
-
-    int Rear() {
-        if (!isEmpty()) {
-            return values[end_p == 0 ? max_capacity - 1 : end_p - 1];
+    int openLock(vector<string>& deadends, string target) {
+        if (target == "0000") return 0;
+        unordered_map<string, string> hash = unordered_map<string, string>();
+        queue<string> q = queue<string>();
+        q.push("0000");
+        for (auto iter = deadends.cbegin(); iter != deadends.cend(); iter++) {
+            hash[*iter] = "00";
         }
-        return -100;
+        if (hash.find("0000") == hash.cend()) {
+            hash["0000"] = "00";
+        }
+        else return -1;
+        int step_count = 0;
+        string this_node;
+        string last_node = "00";
+        while (!q.empty()) {
+            this_node = q.front();
+            q.pop();
+            bool target_founded = find_nearby(hash, q, this_node, target);
+            if (target_founded) return count_steps(hash, target);
+        }
+        return -1;
     }
 
-    bool isEmpty() {
-        if (end_p < start_p) return end_p + max_capacity - start_p == 1;
-        return end_p - start_p == 1;
+    bool find_nearby(unordered_map<string, string>& hash, queue<string>& q, const string& code, const string& target) {
+        string temp = string{ code };
+        for (int i = 0; i != 4; i++) {
+            vector<char> nearby = find_char_nearby(code[i]);
+            for (int j = 0; j != 2; j++) {
+                temp[i] = nearby[j];
+                if (hash.find(temp) == hash.cend()) {
+                    q.push(temp);
+                    hash[temp] = code;
+                }
+                if (temp == target) {
+                    return true;
+                }
+            }
+            temp[i] = code[i];
+        }
+        return false;
     }
 
-    bool isFull() {
-        return start_p == end_p;
+    vector<char> find_char_nearby(const char c) {
+        vector<char> res;
+        res.reserve(2);
+        if (c == '0') {
+            res.emplace_back('9');
+            res.emplace_back('1');
+        }
+        else if (c == '9') {
+            res.emplace_back('0');
+            res.emplace_back('8');
+        }
+        else [[likely]]{
+            res.emplace_back(c - 1);
+            res.emplace_back(c + 1);
+        }
+        return res;
+    }
+
+    int count_steps(unordered_map<string, string>&hash, const string & target) {
+        int steps = 0;
+        string this_node = target;
+        while (true) {
+            this_node = hash[this_node];
+            steps++;
+            if (this_node == "0000") {
+                return steps;
+            }
+        }
+        return -1;
+    }
+};
+
+class Solution_91_recursion_slow {
+public:
+    unordered_map<string, int> hash = unordered_map<string, int>();
+
+    int numDecodings(string s) {
+        if (hash.find(s) != hash.cend()) return hash[s];
+        if (s.size() >= 2) {
+            int res = (check_decode_available(s.substr(0, 1)) ? 1 : 0) * numDecodings(s.substr(1, s.size() - 1))
+                + (check_decode_available(s.substr(0, 2)) ? 1 : 0) * numDecodings(s.substr(2, s.size() - 2));
+            hash[s] = res;
+            return res;
+        }
+        else if (s.size() == 1) {
+            int res = check_decode_available(s) ? 1 : 0;
+            hash[s] = res;
+            return res;
+        }
+        else return 1; // s.size() == 0
+    }
+
+    bool check_decode_available(string s) {
+        // the size of s is not greater than 2
+        if (s.size() == 1) {
+            return s[0] > '0' && s[0] <= '9';
+        }
+        else {
+            return s >= "10" && s <= "26";
+        }
     }
 };
 
@@ -219,3 +275,8 @@ public:
 //   4. 使用错误列表窗口查看错误
 //   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
 //   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+
+int main()
+{
+    cout << "Hello" << endl;
+}
